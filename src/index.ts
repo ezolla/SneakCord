66310fb6a511ee15ca24206bb1fb7e63bbce7213
ezolla <ezoller11@gmail.com>
2020-08-04 21:35:02 -0700

    Added basic akairo setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+require("dotenv").config();
+const { AkairoClient } = require("discord-akairo");
+
+class MyClient extends AkairoClient {
+  constructor() {
+    super(
+      {
+        ownerID: "319625873521115138",
+      },
+      {
+        disableMentions: "everyone",
+      }
+    );
+  }
+}
+
+const client = new MyClient();
+client.login(process.env.TOKEN);
