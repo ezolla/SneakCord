29c73d9b6b30a0089a710a9df6f2576f7d473f6a
ezolla <ezoller11@gmail.com>
2020-08-04 23:11:15 -0700

    Added help command

---
+++ src/commands/help.ts
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const { Command } = require("discord-akairo");
+const paginationEmbed = require("discord.js-pagination");
+
+class PingCommand extends Command {
+  constructor() {
+    super("help", {
+      aliases: ["help"],
+    });
+  }
+
+  pageOne = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle("Help Page 1");
+
+  pageTwo = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle("Help Page 2");
+
+  pageThree = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle("Help Page 2");
+
+  pages = [this.pageOne, this.pageTwo, this.pageThree];
+
+  exec(message: any) {
+    return message.channel.send(paginationEmbed(message, this.pages));
+  }
+}
+
+module.exports = PingCommand;
+export {};
