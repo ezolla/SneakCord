81fa4c2d0b592085dbc0469656d34caa6d80f347
ezolla <ezoller11@gmail.com>
2020-08-04 23:42:29 -0700

    Added about command

---
+++ src/commands/about.ts
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const { Command } = require("discord-akairo");
+
+class AboutCommand extends Command {
+  constructor() {
+    super("about", {
+      aliases: ["about"],
+    });
+  }
+
+  aboutEmbed = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle("SneakCord")
+    .setDescription(
+      "The Discord toolkit you’ll enjoy managing. Providing sneaker focused Discord tooling to your group. \n\n [Website](https://sneakcord.com) - [Twitter](https://twitter.com/SneakCord)"
+    )
+    .setThumbnail(
+      "https://cdn.discordapp.com/attachments/735349347796910090/740439415326900305/sneakcord-discord.png"
+    );
+
+  exec(message: any) {
+    return message.channel.send(this.aboutEmbed);
+  }
+}
+
+module.exports = AboutCommand;
+export {};
