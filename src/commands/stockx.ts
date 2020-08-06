8e167da93fecf0509e198de7e57da1b9ba0b7007
ezolla <ezoller11@gmail.com>
2020-08-05 22:09:58 -0700

    Added stockx command

---
+++ src/commands/stockx.ts
@@ -0,0 +1,80 @@
+const Discord = require("discord.js");
+const { Command } = require("discord-akairo");
+const fetch = require("node-fetch");
+
+class StockxCommand extends Command {
+  constructor() {
+    super("stockx", {
+      aliases: ["stockx"],
+      args: [
+        {
+          id: "search",
+          type: "string",
+          match: "content",
+        },
+      ],
+    });
+  }
+
+  exec(message: any, args: any) {
+    if (args.search) {
+      // Parse search term
+      let searchInjection = args.search.replace(" ", "%20");
+
+      // Search product
+      fetch(
+        "https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.29.0&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6bfb5abee4dcd8cea8f0ca1ca085c2b3",
+        {
+          method: "post",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent":
+              "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+          },
+          body: `{"params":"query=${searchInjection}&hitsPerPage=20&facets=*"}`,
+        }
+      )
+        .then((res: any) => res.json())
+        .then((json: any) => message.channel.send(createEmbed(json.hits[0])));
+    }
+  }
+}
+
+module.exports = StockxCommand;
+export {};
+
+const createEmbed = (product: any) => {
+  let fields = [
+    { name: "Product SKU", value: product.style_id, inline: true },
+    { name: "Colorway", value: product.colorway, inline: true },
+    { name: "Release Date", value: product.traits[3].value, inline: true },
+    { name: "Brand", value: product.brand, inline: true },
+    { name: "Lowest Ask", value: product.lowest_ask, inline: true },
+    { name: "Highest Bid", value: product.highest_bid, inline: true },
+    { name: "Total Sold", value: product.deadstock_sold, inline: true },
+    { name: "Last Sale", value: product.last_sale, inline: true },
+  ];
+
+  // Grabbing dynamic data
+  product.traits.forEach((item: any) => {
+    // Checking for price data
+    if (item.name === "Retail Price") {
+      // Putting price as first field
+      fields.unshift({
+        name: "Retail Price",
+        value: `$${item.value}`,
+        inline: true,
+      });
+    }
+  });
+
+  const embed = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle(product.name)
+    .setURL(`https://stockx.com/${product.url}`)
+    .setThumbnail(product.media.imageUrl)
+    .addFields(fields);
+
+  return embed;
+};
