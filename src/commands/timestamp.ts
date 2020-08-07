1d7a21981f4c2f6095d7fab155c986fe7171e2d0
ezolla <ezoller11@gmail.com>
2020-08-06 22:12:04 -0700

    Added timestamp command

---
+++ src/commands/timestamp.ts
@@ -0,0 +1,65 @@
+const Discord = require("discord.js");
+const { Command } = require("discord-akairo");
+
+const discordEpoch = 1420070400000;
+
+class TimestampCommand extends Command {
+  constructor() {
+    super("timestamp", {
+      aliases: ["timestamp", "snowflake"],
+      args: [
+        {
+          id: "timestamp",
+          type: "string",
+          match: "content",
+        },
+      ],
+    });
+  }
+
+  exec(message: any, args: any) {
+    if (args.timestamp) {
+      const time = new Date(args.timestamp);
+      console.log(time);
+
+      const embed = new Discord.MessageEmbed()
+        .setColor("#5761C9")
+        .addFields(
+          { name: "Unix", value: getUnixTimestamp(args.timestamp) },
+          { name: "ISO 8601", value: getIsoTimestamp(args.timestamp) }
+        );
+
+      return message.channel.send(embed);
+    } else {
+      const embed = new Discord.MessageEmbed()
+        .setColor("#5761C9")
+        .setTitle("Please give timestamp");
+
+      return message.channel.send(embed);
+    }
+  }
+}
+
+module.exports = TimestampCommand;
+export {};
+
+const getUnixTimestamp = (timestamp: any) => {
+  const id = BigInt.asUintN(64, timestamp);
+  const dateBits = Number(id >> 22n);
+
+  const unix = dateBits + discordEpoch;
+  console.log(`UNIX type: ${typeof unix}`);
+
+  return unix.toString();
+};
+
+const getIsoTimestamp = (timestamp: any) => {
+  const id = BigInt.asUintN(64, timestamp);
+  const dateBits = Number(id >> 22n);
+
+  const unix = dateBits + discordEpoch;
+  const iso = new Date(unix).toISOString();
+  console.log(`ISO type: ${typeof iso}`);
+
+  return iso;
+};
