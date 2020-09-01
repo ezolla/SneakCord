799ab9adb6cbd12d38cea5d4b06e3fdf4a47f556
ezolla <ezoller11@gmail.com>
2020-08-31 18:48:17 -0700

    Added webhooks upon events

--- src/listeners/guildCreate.ts
+++ src/listeners/guildCreate.ts
@@ -1,6 +1,8 @@
 const { Listener } = require("discord-akairo");
 const fetch = require("node-fetch");
 
+const { sendWebhook } = require("../utils/webhooks");
+
 class GuildCreateListener extends Listener {
   constructor() {
     super("guildCreate", {
@@ -29,15 +31,75 @@ class GuildCreateListener extends Listener {
       });
 
       if (result.status === 200) {
-        console.log(`Status: ${result.status}`);
+        console.log(`Authenticated server ${guild.name}`);
+
+        // Send webhook
+        sendWebhook(
+          "https://discordapp.com/api/webhooks/750166466766307408/KeXxORemtzUhmHYmBF095_T1uoi7aXmqOPEsiz5mcqu7SwyJ7NCnzpxSuEEfPMdMX_06",
+          {
+            embeds: [
+              {
+                title: "Core Authentication",
+                description: `Tests passed`,
+                color: 5726665,
+                fields: [
+                  {
+                    name: "Name",
+                    value: guild.name,
+                  },
+                  {
+                    name: "ID",
+                    value: guild.id,
+                  },
+                ],
+                footer: {
+                  text: "SneakCord",
+                  icon_url:
+                    "https://cdn.discordapp.com/attachments/735349347796910090/740439415326900305/sneakcord-discord.png",
+                },
+                timestamp: new Date(),
+              },
+            ],
+          }
+        );
       } else {
-        console.log("Failed authenticating server");
+        console.log(`Failed authenticating server ${guild.name}`);
 
         // Ejecting Core instance
         guild
           .leave()
           .then((guild: any) => console.log(`Left the guild ${guild}`))
           .catch(console.error);
+
+        // Send webhook
+        sendWebhook(
+          "https://discordapp.com/api/webhooks/750166466766307408/KeXxORemtzUhmHYmBF095_T1uoi7aXmqOPEsiz5mcqu7SwyJ7NCnzpxSuEEfPMdMX_06",
+          {
+            embeds: [
+              {
+                title: "Core Authentication",
+                description: `Tests failed`,
+                color: 5726665,
+                fields: [
+                  {
+                    name: "Name",
+                    value: guild.name,
+                  },
+                  {
+                    name: "ID",
+                    value: guild.id,
+                  },
+                ],
+                footer: {
+                  text: "SneakCord",
+                  icon_url:
+                    "https://cdn.discordapp.com/attachments/735349347796910090/740439415326900305/sneakcord-discord.png",
+                },
+                timestamp: new Date(),
+              },
+            ],
+          }
+        );
       }
     } else {
       console.log(`Error: ${error}`);
