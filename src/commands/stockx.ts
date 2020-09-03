19f50f639e26a6d422223205cd6777442c78d742
ezolla <ezoller11@gmail.com>
2020-09-02 23:36:36 -0700

    Changed request method capitalization

--- src/commands/stockx.ts
+++ src/commands/stockx.ts
@@ -63,7 +63,7 @@ const getData = async (search: string) => {
   const response = await fetch(
     "https://xw7sbct9v6-dsn.algolia.net/1/indexes/products/query?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%203.29.0&x-algolia-application-id=XW7SBCT9V6&x-algolia-api-key=6bfb5abee4dcd8cea8f0ca1ca085c2b3",
     {
-      method: "post",
+      method: "POST",
       headers: {
         "User-Agent": await randomUseragent.getRandom(),
       },
