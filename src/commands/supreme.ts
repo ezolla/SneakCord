503162374110a4f3ee6aff51cf8de08ca6318db7
ezolla <ezoller11@gmail.com>
2020-09-05 17:02:07 -0700

    Added supreme preview command

--- src/commands/supreme.ts
+++ src/commands/supreme.ts
@@ -58,7 +58,7 @@ class SupremeCommand extends Command {
       }
     }
 
-    // Identifying shopify-specific command
+    // Identifying supreme-specific command
     if (command === "droplist") {
       // Run Supreme droplist scraper
       console.log("Running Supreme droplist scraper");
@@ -115,6 +115,21 @@ class SupremeCommand extends Command {
     } else if (command === "preview") {
       // Run Supreme sellout time scraper
       console.log("Running Supreme preview scraper");
+
+      let items: any;
+
+      // Fetch all data
+      try {
+        items = await getPreview();
+      } catch (err) {
+        console.log(err);
+      }
+
+      // Create and structure embed
+      let embed = await createPreviewEmbed(items);
+
+      // Sending embed to requester channel
+      return paginationEmbed(message, embed);
     } else {
       console.log(argument);
     }
@@ -231,6 +246,38 @@ const getLookbook = async () => {
   return lookbookItems;
 };
 
+// Fetches preview data
+const getPreview = async () => {
+  let previewItems: any = [];
+
+  // Converting response to html
+  const $ = await rp(
+    options("https://www.supremenewyork.com/previews/fallwinter2020")
+  );
+
+  // Loop through preview items
+  $("#container li").each((index: any, data: any) => {
+    let item: any = { id: 1 };
+
+    // Scrape available data
+    if (index) {
+      item.id = index + 1;
+    }
+    if (cheerio.load(data).root().find("a").attr("href")) {
+      item.href = cheerio.load(data).root().find("a").attr("href");
+    }
+    if ($(data).find("img").attr("src")) {
+      item.src = $(data).find("img").attr("src");
+    }
+
+    // Push items into array
+    previewItems.push(item);
+  });
+
+  // Returning array of lookbook items
+  return previewItems;
+};
+
 // Structures droplist embed
 const createDroplistEmbed = async (items: any, droplist: any) => {
   let pages: any = [];
@@ -314,6 +361,51 @@ const createLookbookEmbed = async (items: any) => {
     }
     if (item.alt) {
       embedPage.addField("Item Highlights", item.alt, true);
+    }
+    if (item.src) {
+      embedPage.setImage(`https:${item.src}`);
+    }
+
+    // Pushing embed page
+    pages.push(embedPage);
+  });
+
+  // Return structured embed pages
+  return pages;
+};
+
+// Structures preview embed
+const createPreviewEmbed = async (items: any) => {
+  let pages: any = [];
+
+  // Adding introduction page
+  const embedIntro = new Discord.MessageEmbed()
+    .setColor("#5761C9")
+    .setTitle("Supreme Preview FW20")
+    .setURL("https://www.supremenewyork.com/previews/fallwinter2020")
+    .setDescription(
+      "Browse through the embed pages to see groups of upcoming items in this Supreme season!"
+    );
+
+  // Pushing introductory page
+  pages.push(embedIntro);
+
+  // Looping through each item
+  items.forEach((item: any) => {
+    // Create embed
+    const embedPage = new Discord.MessageEmbed().setColor("#5761C9");
+
+    // Checking and inputting dynamic data
+    if (item.id) {
+      embedPage.setTitle(
+        `Preview Group ${item.id} - ${
+          item.href.split("/")[3].replace("-", " ").charAt(0).toUpperCase() +
+          item.href.split("/")[3].replace("-", " ").slice(1)
+        }`
+      );
+    }
+    if (item.href) {
+      embedPage.setURL(`https://www.supremenewyork.com${item.href}`);
     }
     if (item.src) {
       embedPage.setImage(`https:${item.src}`);
